937ca51500b09806c88c517cdc35d5a7355a552f
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-05-04 11:08:19 +0200

    Made search function work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,48 @@
 import React, {Component} from 'react'
-import auth from '../authorization/auth'
 import {observer} from "mobx-react";
+import {hashHistory} from "react-router";
+import Search from "../stores/Search";
 
 const Home = observer(class Home extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+        let searchParams = {
+            from: "CPH",
+            to: "XXX",
+            date: this.getToday(),
+            tickets: 1
+        };
+
+
+        this.state = {
+            searchParams: searchParams
+        };
+    }
+
+    handleChange = (event) => {
+        let searchParams = this.state.searchParams;
+        let id = event.target.id;
+        if (id === "from") {
+            searchParams.from = event.target.value;
+        } else if (id === "to") {
+            searchParams.to = event.target.value;
+        } else if (id === "date") {
+            searchParams.date = event.target.value;
+        } else if (id === "tickets") {
+            searchParams.tickets = event.target.value;
+        }
+        this.setState({searchParams: searchParams});
+    };
+
+    searchBtn = (event) => {
+        Search.getData(this.state.searchParams);
+
+        event.preventDefault();
+        hashHistory.push('/somewhere');
+    };
 
     getToday = () => {
         let d = new Date();
@@ -18,89 +58,106 @@ const Home = observer(class Home extends Component {
                 </h3>
                 <form className="form-inline">
 
-                    <div className="form-group"><label htmlFor="from"> From:</label><select className="form-control">
-                        <option value="CPH">Copenhagen</option>
-                        <option value="ATL">Atlanta, USA</option>
-                        <option value="PEK">Beijing</option>
-                        <option value="ORD">Chicago, USA</option>
-                        <option value="LHR">London, Heathrow</option>
-                        <option value="HND">Tokyo</option>
-                        <option value="LAX">Los Angeles, USA</option>
-                        <option value="CDG">Paris</option>
-                        <option value="DFW">Dallas/ Fort Worth, USA</option>
-                        <option value="FRA">Frankfurt</option>
-                        <option value="DEN">Denver, USA</option>
-                        <option value="HKG">Hong Kong</option>
-                        <option value="MAD">Madrid</option>
-                        <option value="DXB">Dubai</option>
-                        <option value="JFK">New York, USA</option>
-                        <option value="AMS">Amsterdam</option>
-                        <option value="CGK">Jakarta</option>
-                        <option value="BKK">Bangkok</option>
-                        <option value="SIN">Singapore</option>
-                        <option value="CAN">Guangzhou, China</option>
-                        <option value="PVG">Shanghai, China</option>
-                        <option value="IAH">Houston, USA</option>
-                        <option value="LAS">Las Vegas, USA</option>
-                        <option value="SFO">San Fransisco, USA</option>
-                        <option value="PHX">Phoenix, USA</option>
-                        <option value="CLT">Charlotte, USA</option>
-                        <option value="FCO">Rome</option>
-                        <option value="SYD">Sydney</option>
-                        <option value="MIA">Miami, USA</option>
-                        <option value="MCO">Orlando, USA</option>
-                        <option value="MUC">Munich</option>
-                    </select></div>
-
-                    <div className="form-group"><label htmlFor="to"> To:</label><select className="form-control" defaultValue="XXX">
-                        <option value="XXX">Anywhere</option>
-                        <option value="CPH">Copenhagen</option>
-                        <option value="ATL">Atlanta, USA</option>
-                        <option value="PEK">Beijing</option>
-                        <option value="ORD">Chicago, USA</option>
-                        <option value="LHR">London, Heathrow</option>
-                        <option value="HND">Tokyo</option>
-                        <option value="LAX">Los Angeles, USA</option>
-                        <option value="CDG">Paris</option>
-                        <option value="DFW">Dallas/ Fort Worth, USA</option>
-                        <option value="FRA">Frankfurt</option>
-                        <option value="DEN">Denver, USA</option>
-                        <option value="HKG">Hong Kong</option>
-                        <option value="MAD">Madrid</option>
-                        <option value="DXB">Dubai</option>
-                        <option value="JFK">New York, USA</option>
-                        <option value="AMS">Amsterdam</option>
-                        <option value="CGK">Jakarta</option>
-                        <option value="BKK">Bangkok</option>
-                        <option value="SIN">Singapore</option>
-                        <option value="CAN">Guangzhou, China</option>
-                        <option value="PVG">Shanghai, China</option>
-                        <option value="IAH">Houston, USA</option>
-                        <option value="LAS">Las Vegas, USA</option>
-                        <option value="SFO">San Fransisco, USA</option>
-                        <option value="PHX">Phoenix, USA</option>
-                        <option value="CLT">Charlotte, USA</option>
-                        <option value="FCO">Rome</option>
-                        <option value="SYD">Sydney</option>
-                        <option value="MIA">Miami, USA</option>
-                        <option value="MCO">Orlando, USA</option>
-                        <option value="MUC">Munich</option>
-                    </select></div>
-
-                    <div className="form-group"><label htmlFor="date"> Flight date:</label> <input
-                        className="form-control" id="date" type="date"
-                        min={this.getToday()}
-                        max="2017-12-31"/>
-                    </div>
-                    <div className="form-group"><label htmlFor="tickets"> Tickets:</label> <input
-                        className="form-control"
-                        id="tickets"
-                        type="number" required
-                        min="1" max="25"/>
-                    </div>
                     <div className="form-group">
-                    <input className="form-control" type="button" value="Find"/>
+                        <label htmlFor="from">From:</label>
+                        <select id="from"
+                                className="form-control"
+                                onChange={this.handleChange}>
+                            <option value="CPH">Copenhagen</option>
+                            <option value="ATL">Atlanta, USA</option>
+                            <option value="PEK">Beijing</option>
+                            <option value="ORD">Chicago, USA</option>
+                            <option value="LHR">London, Heathrow</option>
+                            <option value="HND">Tokyo</option>
+                            <option value="LAX">Los Angeles, USA</option>
+                            <option value="CDG">Paris</option>
+                            <option value="DFW">Dallas/ Fort Worth, USA</option>
+                            <option value="FRA">Frankfurt</option>
+                            <option value="DEN">Denver, USA</option>
+                            <option value="HKG">Hong Kong</option>
+                            <option value="MAD">Madrid</option>
+                            <option value="DXB">Dubai</option>
+                            <option value="JFK">New York, USA</option>
+                            <option value="AMS">Amsterdam</option>
+                            <option value="CGK">Jakarta</option>
+                            <option value="BKK">Bangkok</option>
+                            <option value="SIN">Singapore</option>
+                            <option value="CAN">Guangzhou, China</option>
+                            <option value="PVG">Shanghai, China</option>
+                            <option value="IAH">Houston, USA</option>
+                            <option value="LAS">Las Vegas, USA</option>
+                            <option value="SFO">San Fransisco, USA</option>
+                            <option value="PHX">Phoenix, USA</option>
+                            <option value="CLT">Charlotte, USA</option>
+                            <option value="FCO">Rome</option>
+                            <option value="SYD">Sydney</option>
+                            <option value="MIA">Miami, USA</option>
+                            <option value="MCO">Orlando, USA</option>
+                            <option value="MUC">Munich</option>
+                        </select>
                     </div>
+
+                    <div className="form-group">
+                        <label htmlFor="to">To:</label>
+                        <select id="to" className="form-control"
+                                defaultValue="XXX"
+                                onChange={this.handleChange}>
+                            <option value="XXX">Anywhere</option>
+                            <option value="CPH">Copenhagen</option>
+                            <option value="ATL">Atlanta, USA</option>
+                            <option value="PEK">Beijing</option>
+                            <option value="ORD">Chicago, USA</option>
+                            <option value="LHR">London, Heathrow</option>
+                            <option value="HND">Tokyo</option>
+                            <option value="LAX">Los Angeles, USA</option>
+                            <option value="CDG">Paris</option>
+                            <option value="DFW">Dallas/ Fort Worth, USA</option>
+                            <option value="FRA">Frankfurt</option>
+                            <option value="DEN">Denver, USA</option>
+                            <option value="HKG">Hong Kong</option>
+                            <option value="MAD">Madrid</option>
+                            <option value="DXB">Dubai</option>
+                            <option value="JFK">New York, USA</option>
+                            <option value="AMS">Amsterdam</option>
+                            <option value="CGK">Jakarta</option>
+                            <option value="BKK">Bangkok</option>
+                            <option value="SIN">Singapore</option>
+                            <option value="CAN">Guangzhou, China</option>
+                            <option value="PVG">Shanghai, China</option>
+                            <option value="IAH">Houston, USA</option>
+                            <option value="LAS">Las Vegas, USA</option>
+                            <option value="SFO">San Fransisco, USA</option>
+                            <option value="PHX">Phoenix, USA</option>
+                            <option value="CLT">Charlotte, USA</option>
+                            <option value="FCO">Rome</option>
+                            <option value="SYD">Sydney</option>
+                            <option value="MIA">Miami, USA</option>
+                            <option value="MCO">Orlando, USA</option>
+                            <option value="MUC">Munich</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="date">Flight date:</label>
+                        <input
+                            className="form-control" id="date" type="date" required
+                            min={this.getToday()}
+                            max="2017-12-31"
+                            onChange={this.handleChange}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="tickets">Tickets:</label>
+                        <input
+                            className="form-control"
+                            id="tickets"
+                            type="number" required
+                            min="1" max="25"
+                            onChange={this.handleChange}/>
+                    </div>
+
+                    <button id="search" className="btn btn-primary" type="submit" onClick={this.searchBtn}>Search</button>
+
                 </form>
             </div>
         )
---
+++ src/stores/Search.js
@@ -0,0 +1,92 @@
+/**
+ * Created by Jamie on 04-05-2017.
+ */
+import {computed, observable, action, useStrict} from "mobx";
+import fetchHelper from "./fetchHelpers"
+const URL = require("../../package.json").serverURL;
+
+useStrict(true);
+class Search {
+    @observable _airlines = [];
+    @observable messageFromServer = "";
+    @observable errorMessage = "";
+
+    @action
+    setErrorMessage = (err) => {
+        this.errorMessage = err;
+    };
+
+    @action
+    getData = (searchParams) => {
+        let tempDate=searchParams.date+" 00:00:00";
+        let date=new Date(tempDate.replace(/-/g,"/"));
+        this.errorMessage = "";
+        this.messageFromServer = "";
+        this._airlines = [];
+        let errorCode = 200;
+        let sp="/";
+        sp+=searchParams.from+"/";
+        if(searchParams.to!=="XXX" && searchParams.to)sp+=searchParams.to+"/";
+        sp+=date.toISOString()+"/";
+        sp+=searchParams.tickets;
+        const options = fetchHelper.makeOptions("GET", true);
+        fetch(URL + "api/flights"+sp, options)
+            .then((res) => {
+                if (res.status > 210 || !res.ok) {
+                    errorCode = res.status;
+                }
+                return res.json();
+            })
+            .then(action((res) => {  //Note the action wrapper to allow for useStrict
+                if (errorCode !== 200) {
+                    throw new Error(`${res.error.message} (${res.error.code})`);
+                }
+                else {
+                    this._airlines = res;
+                    console.log(this._airlines);
+                }
+            })).catch(err => {
+            //This is the only way (I have found) to verify server is not running
+            this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
+        })
+    };
+
+    @computed get airlineCount() {
+        return this._airlines.length;
+    }
+
+    @computed get airlines() {
+        return this._airlines;
+    }
+
+    @action addBook(airline) {
+        this.errorMessage = "";
+        this.messageFromServer = "";
+        let errorCode = 200;
+        let options = fetchHelper.makeOptions("POST", true);
+
+        options.body = JSON.stringify(airline);
+        fetch(URL + "api/airline", options)
+            .then((res) => {
+                if (res.status > 210 || !res.ok) {
+                    errorCode = res.status;
+                }
+                return res.json();
+            })
+            .then(action((res) => {  //Note the action wrapper to allow for useStrict
+                if (errorCode !== 200) {
+                    throw new Error(`${res.error.message} (${res.error.code})`);
+                }
+                else {
+                    console.log(res);
+                    this._airlines.push(res);
+                    console.log(this._airlines);
+                }
+            })).catch(err => {
+            //This is the only way (I have found) to verify server is not running
+            this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
+        })
+    }
+}
+
+export default new Search();
